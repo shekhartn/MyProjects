32864c6bb778ff30ede65296540d68c9ac03d695
shekhartn <35296366+shekhartn@users.noreply.github.com>
2018-09-10 15:14:21 +0530

    for loop added

--- SampleProject/src/com/sample/Test.java
+++ SampleProject/src/com/sample/Test.java
@@ -3,5 +3,8 @@ package com.sample;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("Hello world!");
+	for(int i=0;i<10;i++){
+	Sysytem.out.println("i="+(i+1));
+	}
 }
 }
