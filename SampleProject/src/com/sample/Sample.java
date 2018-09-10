90cf6576f0144f06ba0f792b6759a9df522718eb
shekhar_t <shekharthr@gmail.com>
2018-09-10 16:00:23 +0530

    Sample and Test files will be changed

--- SampleProject/src/com/sample/Sample.java
+++ SampleProject/src/com/sample/Sample.java
@@ -1,7 +1,6 @@
-com.test
-
-public class Test{
+package com.sample;
+public class Sample{
 public void m1(){
-System.out.println("Displaying m1 method")
+System.out.println("Displaying m1 method");
 }
 }
