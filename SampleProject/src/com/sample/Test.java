8403ff54188a33a278d205cf41d5cd27da6cb680
shekhar_t <shekharthr@gmail.com>
2018-09-10 15:31:53 +0530

    added displaying statement

--- SampleProject/src/com/sample/Test.java
+++ SampleProject/src/com/sample/Test.java
@@ -3,8 +3,9 @@ package com.sample;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("Hello world!");
+	System.out.println("Displaying 1 to 10 numbers");
 	for(int i=0;i<10;i++){
-	Sysytem.out.println("i="+(i+1));
+	System.out.println("i="+(i+1));
 	}
 }
 }
